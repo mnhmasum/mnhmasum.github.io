bbc81534e5e7a13a5e28f440c23a38fb8649d079
masum <nhasan@ael-bd.com>
2019-01-24 16:01:04 +0600

    Updates

---
+++ precache-manifest.b538fe0fbb7dcfffd807548e015b74c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mnhmasum.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5ad9d94941fedc0d3f1b",
+    "url": "/mnhmasum.github.io/static/js/runtime~main.5ad9d949.js"
+  },
+  {
+    "revision": "9527df5b4a53e5463ff5",
+    "url": "/mnhmasum.github.io/static/js/main.9527df5b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/mnhmasum.github.io/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "9527df5b4a53e5463ff5",
+    "url": "/mnhmasum.github.io/static/css/main.37525324.chunk.css"
+  },
+  {
+    "revision": "4996136e85827082092cfcce8bf2055d",
+    "url": "/mnhmasum.github.io/index.html"
+  }
+];
