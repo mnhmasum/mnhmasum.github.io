ee7c9b85a22bc0318ac658e13e73b27e3e242b80
Masum <mnhmasum@gmail.com>
2019-02-10 11:52:23 +0600

    Updates

---
+++ precache-manifest.163bf25f693d7621de023206918fdc22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f05a5143dcb39374354",
+    "url": "/reactice/static/js/runtime~main.4f05a514.js"
+  },
+  {
+    "revision": "10e0e0273c69a4da120f",
+    "url": "/reactice/static/js/main.10e0e027.chunk.js"
+  },
+  {
+    "revision": "b11272c99afd270a83ec",
+    "url": "/reactice/static/js/1.b11272c9.chunk.js"
+  },
+  {
+    "revision": "10e0e0273c69a4da120f",
+    "url": "/reactice/static/css/main.63667fd2.chunk.css"
+  },
+  {
+    "revision": "234892f0578c79b34f38669dbe8d6eb0",
+    "url": "/reactice/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactice/precache-manifest.22a59e4e30e5932714e27c9713304f8b.js"
+  "/reactice/precache-manifest.163bf25f693d7621de023206918fdc22.js"
 );
 
 workbox.clientsClaim();
