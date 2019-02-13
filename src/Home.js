f60b278bab8488288932351c3e161b9f8648f040
masum <nhasan@ael-bd.com>
2019-02-13 15:41:42 +0600

    Change color

--- src/Home.js
+++ src/Home.js
@@ -3,12 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class Home extends React.Component {
-    render() {
-       return (
-          <div>
-             <h1>Home...</h1>
-          </div>
-       )
-    }
-  }
-  export default Home;
+   render() {
+      return (
+         <div>
+            <h1>REACTICE SOFTWARE INFORMATION PROVIDER</h1>
+            <p>
+               Reactice is a free github page which use to provide my personal application and my thinking information.
+             </p>
+
+            <p>
+               <div>Maintained by:</div>
+               Nazmul Hasan Masum
+                </p>
+         </div>
+      )
+   }
+}
+export default Home;
