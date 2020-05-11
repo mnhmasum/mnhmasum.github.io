45b65694452f4a97d35d2f3bf611e6d29eafec13
Nazmul <nhasan@ael-bd.com>
2020-05-11 12:47:57 +0600

    Updates

--- precache-manifest.a7bc0bc14f8ffe921e6142ce5cca867f.js
+++ precache-manifest.a7bc0bc14f8ffe921e6142ce5cca867f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5edf32766e50f2995a4a908860571784",
+    "revision": "9d007fec9ffdfc617098c7079674e9a8",
     "url": "/reactice/index.html"
   },
   {
-    "revision": "c571c90903be3439f3d5",
+    "revision": "468d7864789b7748bf44",
     "url": "/reactice/static/css/main.2487d8dc.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactice/static/js/2.94d7ec6c.chunk.js"
   },
   {
-    "revision": "c571c90903be3439f3d5",
-    "url": "/reactice/static/js/main.8e13ab92.chunk.js"
+    "revision": "468d7864789b7748bf44",
+    "url": "/reactice/static/js/main.38a42a41.chunk.js"
   },
   {
     "revision": "3400ad4c90de8efe1d23",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactice/precache-manifest.4dcd2d6bba0f95feead72297593c6f0c.js"
+  "/reactice/precache-manifest.a7bc0bc14f8ffe921e6142ce5cca867f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactice/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
